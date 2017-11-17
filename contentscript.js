0d5ec382a06215b1e873a4e528180e5011f23e0c
Sid Vishnoi <sidvishnoi8@gmail.com>
2017-11-17 21:24:10 +0530

    Add option to blacklist groups

--- contentscript.js
+++ contentscript.js
@@ -1,11 +1,64 @@
+const asyncParallel = function (tasks, cb) {
+  var results, pending, keys
+  var isSync = true
+
+  if (Array.isArray(tasks)) {
+    results = []
+    pending = tasks.length
+  } else {
+    keys = Object.keys(tasks)
+    results = {}
+    pending = keys.length
+  }
+
+  function done (err) {
+    function end () {
+      if (cb) cb(err, results)
+      cb = null
+    }
+    if (isSync) process.nextTick(end)
+    else end()
+  }
+
+  function each (i, err, result) {
+    results[i] = result
+    if (--pending === 0 || err) {
+      done(err)
+    }
+  }
+
+  if (!pending) {
+    // empty
+    done(null)
+  } else if (keys) {
+    // object
+    keys.forEach(function (key) {
+      tasks[key](function (err, result) { each(key, err, result) })
+    })
+  } else {
+    // array
+    tasks.forEach(function (task, i) {
+      task(function (err, result) { each(i, err, result) })
+    })
+  }
+
+  isSync = false
+}
+
 // get access token from saved extension settings
 function getAccessToken(callback) {
   chrome.storage.sync.get('apiKey', (res) => {
     if (!res.apiKey) {
       alert('API key not set.');
-      throw 'API key not set.'
+      return callback('API key not set.')
     };
-    callback(res.apiKey);
+    callback(null, res.apiKey);
+  });
+}
+
+function getBlacklistedGroups(callback) {
+  chrome.storage.sync.get('groups', (res) => {
+    callback(null, res.groups || []);
   });
 }
 
@@ -19,12 +72,16 @@ function injectScriptFile(fname, callback) {
   (document.head||document.documentElement).appendChild(s);
 }
 
-console.log('injecting..')
-getAccessToken((apiKey) => {
+console.log('injecting..');
+asyncParallel({
+  getAccessToken,
+  getBlacklistedGroups,
+}, (err, results) => {
   // inject scripts to page https://stackoverflow.com/questions/9515704/insert-code-into-the-page-context-using-a-content-script
   var script = document.createElement('script');
   script.textContent = `var fbDevInterest = {
-    API_KEY: '${apiKey}', // make api key available to injected script
+    API_KEY: '${results.getAccessToken}',
+    BLACKLIST: [${results.getBlacklistedGroups}],
     PREFERENCES: {},
     }`;
   (document.head||document.documentElement).appendChild(script);
@@ -33,5 +90,4 @@ getAccessToken((apiKey) => {
   injectScriptFile('utils.js', function(){
     injectScriptFile('fb-dev-merge.js');
   });
-
 });
--- options/options.js
+++ options/options.js
@@ -16,20 +16,47 @@ function saveApikey(e) {
   console.log(apiKey);
 }
 
-// const request = new Request('https://www.facebook.com/pg/DeveloperCircles/groups/', {
-//   mode: 'cors',
-//   redirect: 'follow',
-// });
-// fetch(request)
-//   .then((res) => res.text)
-//   .then(console.log)
-//   .catch((err) => console.error)
 
-// get group from page (scrape)
-// str = ''
-// for (const group of $$('._266w a')) {
-//   const name = group.innerText;
-//   const id = group.getAttribute('data-hovercard').split("?id=")[1].split("&")[0];
-//   const url = group.href;
-//   console.log(name, id, url);
-// }
+// Blacklist groups
+
+const ALL_GROUPS = [{ "id": "249598592040574", "name": "Facebook Developer Circle: Delhi, NCR" }, { "id": "1378294582253698", "name": "Facebook Developer Circle: Nsukka" }, { "id": "2224932161064321", "name": "Facebook Developer Circle: Tunis" }, { "id": "1924443867832338", "name": "Facebook Developer Circle: Bangkok" }, { "id": "1922538421363451", "name": "Facebook Developer Circle: Santiago" }, { "id": "1920036621597031", "name": "Facebook Developer Circle: Malang" }, { "id": "1903916609822504", "name": "Facebook Developer Circle: Ado-Ekiti" }, { "id": "1841081392797911", "name": "Facebook Developer Circle: Surabaya" }, { "id": "1806620552895262", "name": "Facebook Developer Circle: Porto Alegre" }, { "id": "1780072415645281", "name": "Facebook Developer Circle: Chennai" }, { "id": "1741843536047014", "name": "Facebook Developer Circle: Kathmandu" }, { "id": "1724152667880378", "name": "Facebook Developer Circle: Berlin" }, { "id": "1607133026028061", "name": "Facebook Developer Circle: Accra" }, { "id": "1494181493938081", "name": "Facebook Developer Circle: Islamabad" }, { "id": "1443394385967980", "name": "Facebook Developer Circle: Kampala" }, { "id": "1258355007573190", "name": "Facebook Developer Circle: Cairo" }, { "id": "1152576018114322", "name": "Facebook Developer Circle: Bengaluru" }, { "id": "1148469218498930", "name": "Facebook Developer Circle: Lagos" }, { "id": "1075017422642967", "name": "Facebook Developer Circle: Ciudad de Guatemala" }, { "id": "1074858042611323", "name": "Facebook Developer Circle: Hyderabad" }, { "id": "1071045349642536", "name": "Facebook Developer Circle: Lahore" }, { "id": "1041205739348709", "name": "Facebook Developer Circle: Bogotá" }, { "id": "893652180764182", "name": "Facebook Developer Circle: Santa Rita do Sapucaí" }, { "id": "886251554842166", "name": "Facebook Developer Circle: São Paulo" }, { "id": "885490321621308", "name": "Facebook Developer Circle: Harare" }, { "id": "854314664699156", "name": "Facebook Developer Circle: Ho Chi Minh City" }, { "id": "826341790867138", "name": "Facebook Developer Circle: Guadalajara" }, { "id": "813879575430133", "name": "Facebook Developer Circle: Taipei" }, { "id": "811281355669013", "name": "Facebook Developer Circle: Dhaka" }, { "id": "793016410839401", "name": "Facebook Developer Circle: Karachi" }, { "id": "786453984830109", "name": "Facebook Developer Circle: Mumbai" }, { "id": "638854212931776", "name": "Facebook Developer Circle: Manila" }, { "id": "485698195138488", "name": "Facebook Developer Circle: Lille" }, { "id": "476463749198108", "name": "Facebook Developer Circle: Ciudad de México" }, { "id": "428973767504677", "name": "Facebook Developer Circle: Bali" }, { "id": "402137910152010", "name": "Facebook Developer Circle: Bandung" }, { "id": "362906487478469", "name": "Facebook Developer Circle: Geneva" }, { "id": "348458995586076", "name": "Facebook Developer Circle: Amman" }, { "id": "332006040559709", "name": "Facebook Developer Circle: Oldenburg" }, { "id": "313087542449350", "name": "Facebook Developer Circle: Jakarta" }, { "id": "309450039518404", "name": "Facebook Developer Circle: Vienna" }, { "id": "304477986647756", "name": "Facebook Developer Circle: Uyo" }, { "id": "293458267749614", "name": "Facebook Developer Circle: Yogyakarta" }, { "id": "265793323822652", "name": "Facebook Developer Circle: Casablanca" }, { "id": "223094988221674", "name": "Facebook Developer Circle: Buea" }, { "id": "199036970608482", "name": "Facebook Developer Circle: Paris" }, { "id": "187217085094857", "name": "Facebook Developer Circle: Dakar" }, { "id": "186924858495604", "name": "Facebook Developer Circle: Kolkata" }, { "id": "160941794378470", "name": "Facebook Developer Circle: Gaza" }, { "id": "152127978670639", "name": "Facebook Developer Circle: Cape Town" }, { "id": "132580147377707", "name": "Facebook Developer Circle: Buenos Aires" }, { "id": "125327974795168", "name": "Facebook Developer Circle: Madrid" }, { "id": "111858152705945", "name": "Facebook Developer Circle: Campinas" }]
+
+const limit_groups_form = document.getElementById('limit_groups'),
+  limit_groups_list = document.querySelector('#limit_groups #groups'),
+  limit_groups_form_submit = document.querySelector('form#limit_groups button');
+
+const addOption = ({ id, name }, selected) => {
+  const option = document.createElement('option');
+  option.value = id;
+  option.selected = selected;
+  option.innerText = `${name.split(':')[1]} - ${name}`;
+  limit_groups_list.appendChild(option);
+};
+
+chrome.storage.sync.get('groups', (response) => {
+  if (response.groups) {
+    let blacklistedCount = 0;
+    for (const group of ALL_GROUPS) {
+      const selected = response.groups.includes(group.id);
+      if (selected) blacklistedCount++;
+      addOption(group, selected);
+    }
+    document.getElementById('blacklist_summary').innerText = `(Blaclisted ${blacklistedCount}/${ALL_GROUPS.length})`;
+  } else {
+    for (const group of ALL_GROUPS) {
+      addOption(group, false);
+    }
+  }
+});
+
+limit_groups_form.addEventListener('submit', blacklistGroups);
+function blacklistGroups(e) {
+  e.preventDefault();
+  const values = [];
+  for (var i = 0; i < limit_groups_list.options.length; i++) {
+    if (limit_groups_list.options[i].selected) {
+      values.push(limit_groups_list.options[i].value);
+    }
+  }
+  chrome.storage.sync.set({'groups': values}, () => limit_groups_form.groups.classList.add('success'));
+};
